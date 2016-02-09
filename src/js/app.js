dfc036f241deabb735f84bd946e48a291b7ac8d3
danielmoi <daniel@ampersandmoi.com>
2016-02-09 13:26:38 +1100

    ng-init, ng-model, expressions, ng-repeat

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
+// define module using the `angular.module` function
+// the [] parameter can be used to define dependent modules
+// without the [] parameter, we are not creating a new module, but retrieving an existing one
 var app = angular.module('myApp', []);
 
 app.controller('myControl', function($scope) {
